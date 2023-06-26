f7e5990e3bbbb7924148330ac513ab782a1075d5
Marina <marieneflyaway@gmail.com>
2023-06-27 01:07:55 +0200

    avances card cases elementos consola

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,20 @@
 import './Card.css'
+import covidImage from '../../assets/covid-blue.svg'
 
 
-function Card({ title, counter }) {
+function Card({ title, counter, color }) {
     return (
         <>
             <div className="card">
-                <div className="card-title">{title}</div>
-                <div className="card-counter">{counter}</div>
+                <div className="card-title">{title}
+                <img src='http://www.w3.org/2000/svg' alt=""/></div>
+                <div
+                className="card-counter"
+                style={{color: color}}>
+                    {counter}
+                </div>
                 <div className="card-icon">
-                    <img src="covid-blue.svg" alt=""/>
+                    <img src={covidImage} alt=""/>
                 </div>
             </div>
         </>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,12 +5,15 @@ function Main() {
     return(
         <>
             <div id="main">
-                <Card title='Total Cases' counter='1231324124'/>
-                <Card title='Total Deaths' counter='212341'/>
-                <Card title='Total Recovered' counter='3123421'/>
-                <Card title='Total Active' counter='4123412'/>
-                <Card title='New Cases' counter='44312'/>
-                <Card title='New Deaths' counter='123414'/>
+                <div id="cards-container">
+                    <Card title='Total Cases' counter='1231324124' color = 'purpple'/>
+                    <Card title='Total Deaths' counter='212341' color = 'red'/>
+                    <Card title='Total Recovered' counter='3123421' color = 'green'/>
+                    <Card title='Total Active' counter='4123412' color = 'blue'/>
+                    <Card title='New Cases' counter='44312' color = 'orange'/>
+                    <Card title='New Deaths' counter='123414' color = 'dark-red'/>
+                </div>
+                <div></div>
             </div>
         </>
     )
