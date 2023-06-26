6284befd2f2e399246cc444456623dbb45470a0e
lauuloulu <lprsa18@gmail.com>
2023-06-26 15:14:13 +0200

    CT-24: construcción targeta parte 2

--- src/components/cards/cardTop/CardTop.jsx
+++ src/components/cards/cardTop/CardTop.jsx
@@ -1,8 +1,13 @@
 export default function CardTop() {
   return (
 
-    <div>CardTop</div>
-      
+    <div>CardTop
+      <div className="imagen">
+        <img src="" alt="bandera país" />
+      </div>
+      <p className="textpais">Colombia</p>
+      <p className="textdato">7547854786547</p>
+    </div>
 
   )
 }
