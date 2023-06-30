c094b6fa5c4113075a0256beac820f1d31925bec
lauuloulu <lprsa18@gmail.com>
2023-06-30 17:34:06 +0200

    CT-29: todos los componentes están. Falta ordenarlos

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,13 +5,19 @@ function Main() {
     return(
         <>
             <div id="main">
-                <Card title='Title' counter='1234'/>
-                <Card title='Title2' counter='2'/>
-                <Card title='Title3' counter='3'/>
-                <Card title='Title4' counter='4'/>
+                <div id="cards-container">
+                    <Card title='Total Cases' counter='1231324124' color = 'purpple'/>
+                    <Card title='Total Deaths' counter='212341' color = 'red'/>
+                    <Card title='Total Recovered' counter='3123421' color = 'green'/>
+                    <Card title='Total Active' counter='4123412' color = 'blue'/>
+                    <Card title='New Cases' counter='44312' color = 'orange'/>
+                    <Card title='New Deaths' counter='123414' color = 'dark-red'/>
+                </div>
+                <div></div>
             </div>
         </>
     )
 }
+
 
 export default Main
