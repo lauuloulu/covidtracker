30c47ae9234678566e0aca580bc4ae74a08916c1
lauuloulu <lprsa18@gmail.com>
2023-06-30 10:57:39 +0200

    CT-23: Intento 1 de aplicar la API

---
+++ src/Api/Apifooter.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+
+const CovidData = () => {
+    const [cases, setCases]= useState(null);
+    const [deaths, setDeaths]= useState(null);
+    const [recovered, setRecovered]= useState(null);
+
+    useEffect(() => {
+
+const fetchData = async () => {
+    try{
+        const response = await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=1');
+        const data = await response.json();
+
+        setCases(data.cases);
+        setDeaths(data.death);
+        setRecovered(data.recovered);
+    } catch (error) {
+        console.error('Error fetching data', error);
+    }
+};
+
+fetchData();
+
+    }, []);
+
+    return(
+
+    );
+
+    
+};
+
+export default CovidData;
