769208a35188dc25c01d165586ca625829dbab23
MiriV <mirivf@hotmail.es>
2023-07-03 13:33:20 +0200

    resolving

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
 import Nav from './components/nav/Nav'
 import Summary from './components/summary/Summary'
 import Main from './components/main/Main'
-<<<<<<< HEAD
-=======
 import Dropdown from './components/dropdown/Dropdown'
 import Container10 from './components/top10/Container10'
->>>>>>> 38ff1aa4385bc26585f928aafa92e1f034762459
 
 
 function App() {
@@ -15,14 +12,10 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-        <Nav/>
-=======
        
         <Nav/>
         <Container10/>
         <Dropdown/>
->>>>>>> 38ff1aa4385bc26585f928aafa92e1f034762459
         <Summary/>
         <Main/>
     </>
